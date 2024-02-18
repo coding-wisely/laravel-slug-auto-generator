ccb1a728df890dd2d8e0dd96af53d35fee96d44e
Vladimir Nikolic <nezaboravi@gmail.com>
2024-02-18 10:58:44 +0100

    testing github actions

--- src/SlugGeneratorServiceProvider.php
+++ src/SlugGeneratorServiceProvider.php
@@ -10,11 +10,6 @@ class SlugGeneratorServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-slug-auto-generator')
             ->hasConfigFile();
